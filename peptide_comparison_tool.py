a0583d909f2840a91b2c5471de349c6b1bca02f1
sgtkellox <felixlkeller@gmx.de>
2023-03-29 16:22:42 +0200

    MWU

--- peptide_comparison_tool.py
+++ peptide_comparison_tool.py
@@ -1,10 +1,10 @@
 import pandas as pd
 #from openpyxl import load_workbook
 
-pathTable1 = r"E:\Fuat\fuatsTableTest.xlsx"
-pathTable2 = r"E:\Fuat\supp3short.xlsx"
+pathTable1 = r"C:\Users\felix\Desktop\Neuro\Fuat\Med_4_Probe.xlsx"
+pathTable2 = r"C:\Users\felix\Desktop\Neuro\Fuat\Tabelle_Main.xlsx"
 
-pathOut = r"C:\Users\felix\Desktop\fuatTest\result.xlsx"
+pathOut = r"C:\Users\felix\Desktop\Neuro\Fuat\comparisonResult.xlsx"
 
 table1 = pd.read_excel(pathTable1)
 table2 = pd.read_excel(pathTable2)
